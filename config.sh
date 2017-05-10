84f178078ea4f41fbb6eab79a1a0597709cdffb5
Zheng, Ender <crazyender@gmail.com>
2017-05-10 17:00:10 +0800

    Change git config

--- config.sh
+++ config.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 dnf --version > /dev/null
 if [ $? -eq 0 ]; then
-    sudo dnf install clang llvm clang-libs
+    sudo dnf install clang llvm clang-libs cmake
+fi
+
+yum --version > /dev/null
+if [ $? -eq 0 ]; then
+    sudo yum install clang llvm clang-libs cmake
 fi
 
 apt --version > /dev/null
 if [ $? -eq 0 ]; then
-    sudo apt install libclang-dev clang llvm
+    sudo apt install libclang-dev clang llvm cmake
 fi
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/liuchengxu/space-vim/master/install.sh)"
