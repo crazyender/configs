8a6311206c301dee71c898f1b51bc867c9c5f117
Zheng, Ender <ender.zheng@intel.com>
2017-10-13 01:29:56 +0800

    update config files

--- config.sh
+++ config.sh
@@ -14,17 +14,23 @@ if [ $? -eq 0 ]; then
     sudo apt install libclang-dev clang llvm cmake
 fi
 
+apt-get --version > /dev/null
+if [ $? -eq 0 ]; then
+    sudo apt-get install libclang-dev clang llvm cmake
+fi
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/liuchengxu/space-vim/master/install.sh)"
 git clone https://github.com/powerline/fonts.git ~/.fonts
 sh ~/.fonts/install.sh
 vim
-
-~/.vim/plugged/YouCompleteMe/install.py --clang-completer --system-libclang
-
 cp ./.vimrc ~/
 cp ./.spacevim ~/
 cp ./.tmux.conf ~/
 cp ./.gdbinit ~/
 cp ./.bashrc ~/
 cp ./.gitconfig ~/
+vim
+
+~/.vim/plugged/YouCompleteMe/install.py --clang-completer --system-libclang
+
 
--- syncserver.py
+++ syncserver.py
@@ -5,6 +5,42 @@ import watchdog.events
 import watchdog.observers
 import sys
 import os
+import json
+import base64
+
+ignore_extens = [
+        ".o",
+        ".obj",
+        ".a" ,
+        ".bin",
+        ".db",
+        ".ko",
+        ".so",
+        ".tmp",
+        ".TMP",
+        ".d",
+        ".mod.d",
+        ".mod.c",
+        ".cmd"
+]
+
+ignore_keywords = [
+        ".git",
+        "firmware"
+]
+
+def ignore(path):
+        assert isinstance(path, str)
+        for ext in ignore_extens:
+                if path.endswith(ext):
+                        return True
+        for words in ignore_keywords:
+                if path.find(words) != -1:
+                        return True
+        if os.path.isfile(path):
+                if os.stat(path).st_size > (3 *1024*1024):
+                        return True
+        return False
 
 if sys.platform == "linux2":
         import watchdog.observers.inotify
@@ -19,19 +55,24 @@ class LocalFileNotifier(watchdog.events.FileSystemEventHandler):
         def on_created(self, event):
                 if os.path.isdir(event.src_path):
                         return
-
+                if ignore(event.src_path):
+                        return
                 self.center.handle_message("MODIFIED", event.src_path)
 
         def on_deleted(self, event):
                 if os.path.isdir(event.src_path):
                         return
 
+                if ignore(event.src_path):
+                        return
                 self.center.handle_message("DELETE", event.src_path)
 
         def on_modified(self, event):
                 if os.path.isdir(event.src_path):
                         return
 
+                if ignore(event.src_path):
+                        return
                 self.center.handle_message("MODIFIED", event.src_path)
 
         def on_moved(self, event):
@@ -41,27 +82,35 @@ class LocalFileNotifier(watchdog.events.FileSystemEventHandler):
                 if os.path.isdir(event.dest_path):
                         return
 
+                if ignore(event.src_path):
+                        return
+                if ignore(event.dest_path):
+                        return
                 self.center.handle_message("MOVE_FROM", event.src_path)
                 self.center.handle_message("MOVE_TO", event.dest_path)
 
 
 class EventCenter():
         def __init__(self):
-                self.filters = []
-                self.ignores = [".git"]
                 self.remotes = {}
                 self.handlers = {
-                        "REG" : self.handle_reg,
-                        "UNREG" : self.handle_unreg,
-                        "BYE" : self.handle_close,
                         "MODIFIED" : self.handle_modified,
                         "DELETE" : self.handle_delete,
                         "MOVE_FROM" : self.handle_delete,
                         "MOVE_TO" : self.handle_modified,
-                        "SUSPEND" : self.handle_suspend,
-                        "RESUME" : self.handle_resume,
-                        # "SUSPEND" : self.do_nothing,
-                        # "RESUME" : self.do_nothing,
+                        "REMOTE_REG" : self.handle_remote_reg,
+                        "REMOTE_UNREG" : self.handle_remote_unreg,
+                        "REMOTE_BYE" : self.handle_remote_close,
+                        "REMOTE_SUSPEND" : self.handle_remote_suspend,
+                        "REMOTE_RESUME" : self.handle_remote_resume,
+                        "REMOTE_MKDIR" : self.handle_remote_mkdir,
+                        "REMOTE_PUT" : self.handle_remote_put,
+                        "REMOTE_RENAME" : self.handle_remote_rename,
+                        "REMOTE_RM" : self.handle_remote_rm,
+                        "REMOTE_GET" : self.handle_remote_get,
+                        "REMOTE_GET_LIST" : self.handle_remote_get_list,
+                        "REMOTE_EXISTS" : self.handle_remote_exists
+
                 }
                 self.watchers = {}
 
@@ -86,63 +135,151 @@ class EventCenter():
                 print("{0} connected".format(ip))
                 self.remotes[ip] = remote
 
-        def handle_message(self, _id, msg):
+        def handle_message(self, _id, msg, remote=None):
                 assert isinstance(_id,str)
                 assert isinstance(msg, str)
-                if msg in self.filters and _id != "RESUME":
-                        print("{0} {1} but remote dont want it, do nothing".format(_id, msg))
-                        return
-
-                for igonre_path in self.ignores:
-                        if msg.find(igonre_path) != -1:
-                                print("{0} {1} but in ignore list, do nothing".format(_id, msg))
-                                return
 
                 if _id in self.handlers.keys():
-                        print("{0} : {1}".format(_id, msg))
-                        self.handlers[_id](msg)
+                        if _id.startswith("REMOTE") == False:
+                                print("recv:{0}:{1}".format(_id, msg))
+                        self.handlers[_id](msg, remote)
                 else:
                         print("Unknown msg: " + _id)
 
-        def handle_reg(self, msg):
+        def handle_remote_reg(self, msg, remote=None):
                 assert isinstance(msg, str)
+                print("recv:REG:"+msg)
                 w = self.observer.schedule(self.local, msg, True)
                 self.watchers[msg] = w
 
-        def handle_unreg(self, msg):
+        def handle_remote_unreg(self, msg, remote=None):
                 assert isinstance(msg, str)
+                print("recv:UNREG:"+msg)
                 if msg not in self.watchers.keys():
-                        print("{0} not under watch".format(msg))
+                        pass
                 else:
                         self.observer.unschedule(self.watchers[msg])
                         self.watchers.pop(msg)
 
-        def handle_close(self, msg):
+        def handle_remote_close(self, msg, remote=None):
                 assert isinstance(msg, str)
-                self.remotes.pop(msg)
+                print("recv:BYE:"+msg)
+                self.remotes.pop(msg.split(":")[0])
 
-        def handle_modified(self, msg):
+        def handle_modified(self, msg, remote=None):
                 assert isinstance(msg, str)
-                self.send_message("PULL", msg)
+                if os.path.exists(msg) == False:
+                        return
+                elif os.path.islink(msg):
+                        return
+                elif ignore(msg):
+                        return
+                else:
+                        f = open(msg, "r")
+                        c = f.read()
+                        f.close()
+                        data = base64.b64encode(c)
+                obj = {}
+                obj["path"] = msg
+                obj["content_base64"] = data
+                self.send_message("PULL", json.dumps(obj), remote)
 
 
-        def handle_delete(self, msg):
+        def handle_delete(self, msg, remote=None):
                 assert isinstance(msg, str)
                 self.send_message("RM", msg)
 
-        def send_message(self, _id, msg):
+        def send_message(self, _id, msg, remote=None):
                 if len(self.remotes) == 0:
                         print("Error, no remote")
                         return
-                self.remotes.values()[0].sendall(_id + ":" + msg + "\n")
+                if remote == None:
+                        for r in self.remotes.values():
+                                r.sendall(_id + ":" + msg + "\n")
+                else:
+                        remote.sendall(_id+":"+msg+"\n")
 
-        def handle_suspend(self, msg):
-                if msg not in self.filters:
-                        self.filters.append(msg)
+        def handle_remote_suspend(self, msg, remote=None):
+                print("recv:SUSPEND:"+msg)
+                for path in self.watchers.keys():
+                        self.observer.unschedule(self.watchers[path])
 
-        def handle_resume(self, msg):
-                if msg in self.filters:
-                        self.filters.remove(msg)
+        def handle_remote_resume(self, msg, remote=None):
+                print("recv:RESUME:"+msg)
+                for path in self.watchers.keys():
+                        w = self.observer.schedule(self.local, path, True)
+                        self.watchers[path] = w
+
+        def handle_remote_put(self, msg, remote=None):
+                obj = json.loads(msg)
+                path = obj["path"]
+                content_base64 = obj['content_base64']
+                print("recv:PUT:"+path)
+                f = open(path, 'w')
+                data = base64.b64decode(content_base64)
+                f.write(data)
+                f.close()
+
+        def handle_remote_rename(self, msg, remote=None):
+                obj = json.loads(msg)
+                old_path = obj["old_path"]
+                new_path = obj["new_path"]
+                print("recv:RENAME:"+old_path+" -> " + new_path)
+                try:
+                        os.rename(old_path, new_path)
+                except:
+                        pass
+
+        def handle_remote_mkdir(self, msg, remote=None):
+                print("recv:MKDIR:"+msg)
+                try:
+                        os.makedirs(msg)
+                except:
+                        pass
+
+        def handle_remote_rm(self, msg, remote=None):
+                print("recv:RM:"+msg)
+                try:
+                        os.remove(msg)
+                except:
+                        pass
+
+        def handle_remote_get(self, msg, remote=None):
+                print("recv:GET:"+msg)
+                self.handle_modified(msg, remote)
+
+        def handle_remote_get_list(self, msg, remote):
+                print("recv:LIST:"+msg)
+                file_list = []
+                for f in os.listdir(msg):
+                        if ignore(f):
+                                continue
+                        file_content = {}
+                        full_path = os.path.join(msg, f)
+                        if os.path.isfile(full_path):
+                                file_content["is_path"] = False
+                        elif os.path.isdir(full_path):
+                                file_content["is_path"] = True
+                        else:
+                                file_content["is_path"] = False
+
+                        file_content["name"] = f
+                        file_list.append(file_content)
+                ret = json.dumps(file_list)
+                if remote != None:
+                        remote.sendall("LIST:"+ret+"\n")
+
+        def handle_remote_exists(self, msg, remote):
+                print("recv:EXISTS:"+msg)
+                result = os.path.exists(msg)
+                ret = "NO"
+                if result == True:
+                        ret = "YES"
+                else:
+                        ret = "NO"
+                if remote != None:
+                        remote.sendall("EXISTS:"+ret+"\n")
+
 
 event_center = EventCenter()
 
@@ -153,11 +290,17 @@ class ServerHandler(SocketServer.StreamRequestHandler):
                                         self.request)
                 while True:
                         line = self.rfile.readline()
+                        assert(isinstance(line, str))
                         if len(line) == 0:
                                 break
                         line = line.strip()
-                        pair = line.split(":")
-                        event_center.handle_message(pair[0].strip(), pair[1].strip())
+                        pair = []
+                        index = line.find(":")
+                        pair.append(line[0:index])
+                        pair.append(line[index+1:])
+                        event_center.handle_message(pair[0].strip(),
+                                                    pair[1].strip(),
+                                                    self.request)
                         if pair[0].strip() == "BYE":
                                 break
 
