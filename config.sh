a184dd470bc3af1ed8b70be986ae96fceac61d1d
Zheng, Ender <crazyender@gmail.com>
2018-01-26 18:51:46 +0800

    modify vimrc

--- config.sh
+++ config.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 dnf --version > /dev/null
 if [ $? -eq 0 ]; then
-    sudo dnf install clang llvm clang-libs cmake ctags cscope
+    sudo dnf install clang llvm clang-libs cmake ctags cscope id-utils
 fi
 
 yum --version > /dev/null
 if [ $? -eq 0 ]; then
-    sudo yum install clang llvm clang-libs cmake ctags cscope
+    sudo yum install clang llvm clang-libs cmake ctags cscope id-utils
 fi
 
 apt --version > /dev/null
 if [ $? -eq 0 ]; then
-    sudo apt install libclang-dev clang llvm cmake ctags cscope
+    sudo apt install libclang-dev clang llvm cmake ctags cscope id-utils
 fi
 
 apt-get --version > /dev/null
 if [ $? -eq 0 ]; then
-    sudo apt-get install libclang-dev clang llvm cmake ctags cscope
+    sudo apt-get install libclang-dev clang llvm cmake ctags cscope id-utils
 fi
+
+#curl -sLf https://spacevim.org/install.sh | bash
 
 folder=`pwd`
 cd ~/
@@ -27,13 +29,6 @@ git clone https://github.com/exvim/main
 cd main
 sh unix/install.sh
 sh unix/replace-my-vim.sh
-cd ..
-wget https://ftp.gnu.org/gnu/idutils/idutils-4.6.tar.xz
-tar -xf idutils-4.6.tar.xz
-cd idutils-4.6
-./configure
-make
-sudo make install
 cd $folder
 
 cp ./.tmux.conf ~/
@@ -41,4 +36,9 @@ cp ./.gdbinit ~/
 cp ./.bashrc ~/
 cp ./.gitconfig ~/
 
+echo "nnoremap <C-k> <C-W><Up>" >> ~/.vimrc
+echo "nnoremap <C-j> <C-W><Down>" >> ~/.vimrc
+echo "nnoremap <C-h> <C-W><Left>" >> ~/.vimrc
+echo "nnoremap <C-l> <C-W><Right>" >> ~/.vimrc
+echo "nnoremap <TAB> :bn <CR>" >> ~/.vimrc
 echo "nnoremap <unique> <silent> wm :EXProjectToggle<cr>" >> ~/.vimrc
